12eb76261ad37f36329d54250d0084b507db3e3a
sac2277 <sachinnlohar@gmail.com>
2024-10-21 14:18:47 +0530

    first commit

---
+++ src/test/java/com/demo/Demobatch624ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demobatch624ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
